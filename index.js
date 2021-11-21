b2dadddccf633e1a9004d7e333ded9bff4a19810
Marisa Triola <marisatriola@gmail.com>
2021-11-21 18:54:40 -0500

    fix excape characters

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const app = require('express')();
 const axios = require('axios');
+const he = require("he")
+const passages = []
 
 const path = require('path')
 app.set('views', path.join(__dirname, 'views'))
@@ -10,12 +12,18 @@ app.get('/generate', async (req, res) => {
     res.send(postres.data.prompt + postres.data.text);
 })
 
-app.get('/', async (req, res) => {
-    postres = await axios.post("http://api.vicgalle.net:5000/generate");
-    text = postres.data.text
-    res.render('index', { text })
+app.get('/play', async (req, res) => {
+    text = he.decode(passages[req.query.lang].data.text)
+    res.render('play', { text, he })
 })
 
-app.listen(3000, () => {
+app.get('/', (req, res) => {
+    res.render('index');
+})
+
+app.listen(3000, async () => {
+    passages.fr = await axios.post("http://api.vicgalle.net:5000/generate", {}, { params: { "context": "je suis.", "token_max_length": 100 } });
+    passages.en = await axios.post("http://api.vicgalle.net:5000/generate", {}, { params: { "context": "Hello", "token_max_length": 100 } });
+    passages.c = await axios.post("http://api.vicgalle.net:5000/generate", {}, { params: { "context": "#include <stdio.h>", "token_max_length": 100 } });
     console.log("listening")
 })
